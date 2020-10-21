2b5cae1c14b3b5a8556f8c84cae5ab2eb143ec3b
jinujeon <ruldy99@naver.com>
2020-10-21 17:51:52 +0900

    p712 update

---
+++ Android강의/P712/app/src/main/java/com/example/p712/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.p712;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tx;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tx = findViewById(R.id.tx);
+        FirebaseMessaging.getInstance().subscribeToTopic("car").addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                String msg = "FCM Complete ...";
+                if(!task.isSuccessful()){
+                    msg = "FCM Fail";
+                }
+                Log.d("[TAG]", msg);
+            }
+        });
+        LocalBroadcastManager lbm = LocalBroadcastManager.getInstance(this);
+        lbm.registerReceiver(receiver, new IntentFilter("notification"));
+    }   // end onCreate
+
+    public BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent != null){
+                String title = intent.getStringExtra("title");
+                String control = intent.getStringExtra("control");
+                String data = intent.getStringExtra("data");
+                tx.setText(control+" "+data);
+                Toast.makeText(MainActivity.this, title+" "+ control+" "+data, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+}
